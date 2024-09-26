e928e385a7eef96bc344f8ed025065f2f4608126
douglas.martignago <douglas.martignago@useall.com.br>
2024-09-26 00:15:21 -0300

    context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,52 +4,42 @@ import "./globals.css";
 import type { ReactNode } from "react";
 import { ThemeProvider } from "styled-components";
 import { useEffect, useState } from "react";
-import Loading from "./components/Loading";
 import { Button } from "antd";
-
-const lightTheme = {
-  primaryColor: "#1890ff",
-  background: "#ffffff",
-  color: "#000000",
-};
-
-const darkTheme = {
-  primaryColor: "#ff4d4f",
-  background: "#000000",
-  color: "#ffffff",
-};
+import Head from "next/head";
+import { AppProvider, useAppContext } from '@/app/context/AppContext';
+import Loading from "./components/Loading";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  const [theme, setTheme] = useState(lightTheme);
-  const [loading, setLoading] = useState(true);
-
-  const toggleTheme = () => {
-    setTheme((prevTheme) =>
-      prevTheme === lightTheme ? darkTheme : lightTheme
-    );
-  };
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-    return () => clearTimeout(timer);
+    setIsMounted(true);
   }, []);
 
+
   return (
-    <html lang="pt-BR">
-      <body style={{ backgroundColor: theme.background, color: theme.color }}>
-        {loading ? (
-          <div>Carregando...</div>
-        ) : (
-          <ThemeProvider theme={theme}>
-            <Button onClick={toggleTheme} style={{ margin: "20px" }}>
-              Alternar Tema
-            </Button>
-            {loading ? <Loading /> : children}
-          </ThemeProvider>
-        )}
-      </body>
+    <html>
+      <Head>
+        <title>My App</title>
+      </Head>
+      <AppProvider>
+        {!isMounted ? <Loading /> : <BodyContent>{children}</BodyContent>}
+      </AppProvider>
     </html>
+  );
+}
+
+function BodyContent({ children }: { children: ReactNode }) {
+  const { theme, toggleTheme } = useAppContext();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <body style={{ backgroundColor: theme.background, color: theme.color }}>
+        <Button onClick={toggleTheme} style={{ margin: "20px" }}>
+          Alternar Tema
+        </Button>
+        {children}
+      </body>
+    </ThemeProvider>
   );
 }
